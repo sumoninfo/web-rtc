826295e1750f306c32b09e7ed04fde183d83d93e
Ekramul Islam Sumon <mmsumon799@gmail.com>
2022-02-15 16:14:01 +0600

    web rtc with react

---
+++ web_rtc_with_react/index.js
@@ -0,0 +1,22 @@
+const app    = require('express')();
+const server = require('http').createServer(app);
+const cros   = require('cors');
+
+const io = require('socket.io')(server, {
+    cros: {
+        origin : '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cros());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Server is running");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
