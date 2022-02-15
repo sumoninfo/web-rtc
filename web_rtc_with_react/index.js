f888037eb85dc8ef38a1151edc8c36e48cd05ded
Ekramul Islam Sumon <mmsumon799@gmail.com>
2022-02-15 16:23:19 +0600

    server side

--- web_rtc_with_react/index.js
+++ web_rtc_with_react/index.js
@@ -17,6 +17,25 @@ app.get('/', (req, res) => {
     res.send("Server is running");
 });
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded');
+    });
+
+    socket.on('callUser', ({userToCall, signalData, from, name}) => {
+        io.to(userToCall).emit('callUser', {signal: signalData, from, name});
+    });
+
+    socket.on('answerCall', (data) => {
+        io.on(data.to).emit('callAccepted', data.signal);
+    })
+});
+
+
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
